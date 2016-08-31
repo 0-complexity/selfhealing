c1d03a438aa70620c74226316faf8d4757f07c5f
christophe de Carvalho Pereira Martincs <christophe.dcpm@gmail.com>
2016-08-31 10:52:12 +0200

    refactor disk_monitoring. renamed to disk_physical_stats.py
    only send metric we monitor to aggregator

--- jumpscripts/collectors_stats/disk_physical_stats.py
+++ jumpscripts/collectors_stats/disk_physical_stats.py
@@ -2,7 +2,7 @@ from JumpScale import j
 import time
 
 descr = """
-gather statistics about disks
+gather statistics about physical disks
 """
 
 organization = "jumpscale"
@@ -10,15 +10,16 @@ author = "kristof@incubaid.com"
 license = "bsd"
 version = "1.0"
 category = "disk.monitoring"
-period = 300 #always in sec
+period = 300  # always in sec
 timeout = period * 0.2
 order = 1
-enable=True
-async=True
-queue='process'
-log=False
+enable = True
+async = True
+queue = 'process'
+log = False
 
 roles = []
+
 
 def action():
     import JumpScale.lib.diskmanager
@@ -26,45 +27,46 @@ def action():
 
     dcl = j.clients.osis.getCategory(j.core.osis.client, "system", "disk")
     rediscl = j.clients.redis.getByInstance('system')
-    rediscl = j.clients.redis.getByInstance('system')
     aggregatorcl = j.tools.aggregator.getClient(rediscl, "%s_%s" % (j.application.whoAmI.gid, j.application.whoAmI.nid))
 
     disks = j.system.platform.diskmanager.partitionsFind(mounted=True, prefix='', minsize=0, maxsize=None)
 
-    #disk counters
-    counters=psutil.disk_io_counters(True)
+    # disk counters
+    counters = psutil.disk_io_counters(True)
+    now = j.base.time.getTimeEpoch()
     all_results = {}
 
     for disk in disks:
 
         results = {'time_read': 0, 'time_write': 0, 'count_read': 0, 'count_write': 0,
                    'kbytes_read': 0, 'kbytes_write': 0,
+                   'MB_read': 0, 'MB_write': 0,
                    'space_free_mb': 0, 'space_used_mb': 0, 'space_percent': 0}
-        path=disk.path.replace("/dev/","")
+        path = disk.path.replace("/dev/", "")
 
         odisk = dcl.new()
         oldkey = rediscl.hget('disks', path)
         odisk.nid = j.application.whoAmI.nid
         odisk.gid = j.application.whoAmI.gid
 
-        if counters.has_key(path):
-            counter=counters[path]
-            read_count, write_count, read_bytes, write_bytes, read_time, write_time=counter
+        if path in counters:
+            counter = counters[path]
+            read_count, write_count, read_bytes, write_bytes, read_time, write_time = counter
             results['time_read'] = read_time
             results['time_write'] = write_time
             results['count_read'] = read_count
             results['count_write'] = write_count
 
-            read_bytes=int(round(read_bytes/1024,0))
-            write_bytes=int(round(write_bytes/1024,0))
-            results['kbytes_read'] = read_bytes
-            results['kbytes_write'] = write_bytes
+            results['kbytes_read'] = int(round(read_bytes / 1024, 0))
+            results['kbytes_write'] = int(round(write_bytes / 1024, 0))
+            results['MB_read'] = int(round(read_bytes / 1024 * 1024, 0))
+            results['MB_write'] = int(round(write_bytes / 1024 * 1024, 0))
             results['space_free_mb'] = int(round(disk.free))
-            results['space_used_mb'] = int(round(disk.size-disk.free))
-            results['space_percent'] = int(round((float(disk.size-disk.free)/float(disk.size)),2))
+            results['space_used_mb'] = int(round(disk.size - disk.free))
+            results['space_percent'] = int(round((float(disk.size - disk.free) / float(disk.size)), 2))
 
-        for key,value in disk.__dict__.iteritems():
-            odisk.__dict__[key]=value
+        for key, value in disk.__dict__.iteritems():
+            odisk.__dict__[key] = value
 
         ckey = odisk.getContentKey()
         if ckey != oldkey:
@@ -72,10 +74,30 @@ def action():
             dcl.set(odisk)
             rediscl.hset('disks', path, ckey)
 
-        for key, value in results.iteritems():
-            key = "%s_%s_dev_%s_%s" % (j.application.whoAmI.gid, j.application.whoAmI.nid, path, key)
+        # send stats to redis aggregator
+        stats = {
+            'disk.iops.read': {
+                'value_key': 'count_read',
+                'type': 'D'
+            },
+            'disk.iops.write': {
+                'value_key': 'count_write',
+                'type': 'D'
+            },
+            'disk.throughput.read': {
+                'value_key': 'kbytes_read',
+                'type': 'D'
+            },
+            'disk.throughput.write': {
+                'value_key': 'kbytes_write',
+                'type': 'D'
+            }
+        }
+        for key, info in stats.iteritems():
+            key = '%s.phys.%d.%d.%s' % (key, j.application.whoAmI.gid, j.application.whoAmI.nid, path)
             tags = 'gid:%d nid:%d device:%s' % (j.application.whoAmI.gid, j.application.whoAmI.nid, path)
-            aggregatorcl.measure(key, tags, value, timestamp=None)
+            value = results[info['value_key']]
+            aggregatorcl._measure(key, tags, value, type=info['type'], timestamp=now)
 
         all_results[path] = results
 
