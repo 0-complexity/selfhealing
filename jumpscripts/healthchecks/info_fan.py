8af4595982102ff39c33fa4eba3f8621100d5253
Jo De Boeck <deboeck.jo@gmail.com>
2016-09-01 19:29:34 +0200

    Fixes in info_fan script

--- jumpscripts/healthchecks/info_fan.py
+++ jumpscripts/healthchecks/info_fan.py
@@ -9,20 +9,20 @@ author = "thabeta@codescalers.com"
 version = "1.0"
 category = "monitor.healthcheck"
 roles = ['node']
-period = 60 # 1min
+period = 60  # 1min
 enable = True
 async = True
 queue = 'process'
 log = True
 
+
 def action():
     category = "Fans"
     results = []
-    rc, _ = j.system.process.execute("which ipmitool", dieOnNonZeroExitCode=False)
-    if rc != 0:
-        rc, out = j.system.process.execute("""ipmitool sdr type "Fan" """, dieOnNonZeroExitCode=False)
+    rc, out = j.system.process.execute("""ipmitool sdr type "Fan" """, dieOnNonZeroExitCode=False)
+    if rc != 127:  # 127 is command not found
         if out:
-            #SAMPLE:
+            # SAMPLE:
             # root@du-conv-3-01:~# ipmitool sdr type "Fan"
             # FAN1             | 41h | ok  | 29.1 | 5000 RPM
             # FAN2             | 42h | ns  | 29.2 | No Reading
@@ -31,13 +31,13 @@ def action():
 
             for line in out.splitlines():
                 parts = [part.strip() for part in line.split("|")]
-                id_ , sensorstatus, message = parts[0], parts[2], parts[-1]
+                id_, sensorstatus, message = parts[0], parts[2], parts[-1]
                 if sensorstatus == "ns" and "no reading" in message.lower():
-                    results.append(dict(state='SKIPPED', category=category, message="No reading on %s (%s)"%(id_, message)))
+                    results.append(dict(state='SKIPPED', category=category, message="No reading on %s (%s)" % (id_, message)))
                 elif sensorstatus != "ok" and "no reading" not in message.lower():
-                    results.append(dict(state='WARNING', category=category, message="Fan %s has problem (%s)"%(id_, message)))
+                    results.append(dict(state='WARNING', category=category, message="Fan %s has problem (%s)" % (id_, message)))
                 else:
-                    results.append(dict(state='OK', category=category, message="(%s) is OK: %s"%(id_, message)))
+                    results.append(dict(state='OK', category=category, message="(%s) is OK: %s" % (id_, message)))
 
     return results
 
