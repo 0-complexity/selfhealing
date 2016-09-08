c01450816e8bb99def9ad431bda1406ff4c01380
Muhamad Azmy <muhamad.azmy@codescalers.com>
2016-09-08 10:22:31 +0200

    Query now() - 5m instead of -10m
    Also reschedule the script to run every 60s instead of 5m

--- jumpscripts/maintenance/selfhealing_threashold.py
+++ jumpscripts/maintenance/selfhealing_threashold.py
@@ -14,7 +14,7 @@ async = True
 queue = 'process'
 roles = ['statscollector']
 enable = True
-period = 300
+period = 60
 log = True
 
 ###############
@@ -44,7 +44,7 @@ def _aggregate(series, tag):
 
 
 def _process_iops(ovc, influx):
-    result = influx.query('''SELECT mean(value) FROM /disk.iops.*\|m/ WHERE "type" = 'virtual' AND time > now() - 10m GROUP BY "vdiskid"''')
+    result = influx.query('''SELECT mean(value) FROM /disk.iops.*\|m/ WHERE "type" = 'virtual' AND time > now() - 5m GROUP BY "vdiskid"''')
     if 'series' not in result.raw:
         print('disk.iops no data')
         return
@@ -66,13 +66,13 @@ def _process_iops(ovc, influx):
 
 
 def _process_network(ovc, influx):
-    throughput = influx.query('''SELECT mean(value) FROM /network.throughput.*\|m/ WHERE "type" = 'virtual' AND time > now() - 10m GROUP BY "mac"''')
+    throughput = influx.query('''SELECT mean(value) FROM /network.throughput.*\|m/ WHERE "type" = 'virtual' AND time > now() - 5m GROUP BY "mac"''')
     if 'series' not in throughput.raw:
         print('network.throughput no data')
         return
     agg_throughput = _aggregate(throughput.raw['series'], 'mac')
 
-    packet = influx.query('''SELECT value FROM /network.packets.*\|m/ WHERE "type" = 'virtual'  AND time > now() - 10m GROUP BY "mac"''')
+    packet = influx.query('''SELECT value FROM /network.packets.*\|m/ WHERE "type" = 'virtual'  AND time > now() - 5m GROUP BY "mac"''')
     if 'series' not in packet.raw:
         print('network.packets no data')
         return
