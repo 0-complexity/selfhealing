eb1d97d6a397b6033c9a1257da834ff14a55924f
Ahmed T. Youssef <xmonader@gmail.com>
2016-09-21 11:21:12 +0200

    init implementation for routeros_check_schedule.

---
+++ jumpscripts/healthchecks/routeros_check_schedule.py
@@ -0,0 +1,35 @@
+from JumpScale import j
+
+descr = """
+Scheduler that runs on master to check for dead routeros devices.
+
+"""
+
+organization = 'cloudscalers'
+category = "monitor.healthcheck"
+author = "thabeta@codescalers.com"
+version = "1.0"
+
+enable = True
+async = True
+period = 15*60  # 15mins.
+roles = ['master']
+queue = 'process'
+
+
+def action():
+    acl = j.clients.agentcontroller.get()
+    ccl = j.clients.osis.getNamespace('cloudbroker')
+    results = []
+
+    for location in ccl.location.search({})[1:]:
+        job = acl.executeJumpscript('greenitglobe', 'routeros_check', role='cpunode', gid=location['gid'])
+        if job['state'] == 'OK':
+            results.extend(job['result'])
+    if not results:
+        results.append({'state': 'OK', 'category': 'Network', 'message': 'All RouterOS devices are OK.'})
+    return results
+
+if __name__ == '__main__':
+    import yaml
+    print(yaml.dump(action(), default_flow_style=False))
