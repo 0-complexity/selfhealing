a8851eb3b3a32bc359dfc69cdadc42fb186a37d5
Jo De Boeck <deboeck.jo@gmail.com>
2016-09-26 09:59:59 +0200

    make openfd_check more robust

--- jumpscripts/healthchecks/openfd_check.py
+++ jumpscripts/healthchecks/openfd_check.py
@@ -22,16 +22,20 @@ def action():
     results = []
 
     for proc in psutil.get_process_list():
-        state = None
-        soft, hard = proc.rlimit(psutil.RLIMIT_NOFILE)
-        count = proc.num_fds()
-        if count > 0.8 * soft:
-            state = 'WARNING'
-        elif count > 0.9 * soft:
-            state = 'ERROR'
-        if state is not None:
-            msg = "To many open file descriptors for {} with PID {} {}/{}".format(proc.cmdline(), proc.pid, count, soft)
-            results.append({'state': state, 'msg': msg, 'category': category, 'uid': msg})
+        try:
+            state = None
+            soft, hard = proc.rlimit(psutil.RLIMIT_NOFILE)
+            count = proc.num_fds()
+            if count > 0.8 * soft:
+                state = 'WARNING'
+            elif count > 0.9 * soft:
+                state = 'ERROR'
+            if state is not None:
+                msg = "To many open file descriptors for {} with PID {} {}/{}".format(proc.cmdline(), proc.pid, count, soft)
+                results.append({'state': state, 'msg': msg, 'category': category, 'uid': msg})
+        except psutil.NoSuchProcess:
+            # process exited just carry on like it never existed
+            pass
     if len(results) == 0:
         msg = 'Open filedescriptors are all OK'
         results.append({'state': 'OK', 'msg': 'msg', 'category': category})
