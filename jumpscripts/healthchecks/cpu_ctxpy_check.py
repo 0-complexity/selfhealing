b7ea17c53eaa46f96b8ba0c037084077f74bb615
Yves Kerwyn <yves.kerwyn@greenitglobe.com>
2016-10-05 18:43:41 +0200

    Update error messages of cpu_ctxpy_check.py

--- jumpscripts/healthchecks/cpu_ctxpy_check.py
+++ jumpscripts/healthchecks/cpu_ctxpy_check.py
@@ -47,20 +47,20 @@ def action():
         return [result]
 
     avgctx = stat.h_avg
-    result['message'] = 'CPU context switch value is: %.2f/s' % avgctx
+    result['message'] = 'Number of CPU context switches per second: %.2f/s' % avgctx
     level = None
     if avgctx > 1000000:
         level = 1
         result['state'] = 'ERROR'
-        result['uid'] = 'CPU context switch value is too high'
+        result['uid'] = 'Number of CPU context switches per second is too high'
 
     elif avgctx > 600000:
         level = 2
         result['state'] = 'WARNING'
-        result['uid'] = 'CPU context switch value is too high'
+        result['uid'] = 'Number of CPU context switches per second is too high'
 
     if level:
-        msg = 'Number of CPU context switches per second is too high, current value: %.2f/s' % avgctx
+        msg = 'Number of CPU context switches per second is too high: %.2f/s' % avgctx
         eco = j.errorconditionhandler.getErrorConditionObject(msg=msg, category='monitoring', level=level, type='OPERATIONS')
         eco.nid = nid
         eco.gid = gid
