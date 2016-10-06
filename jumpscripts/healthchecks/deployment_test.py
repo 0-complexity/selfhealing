08bd4cd7f2fb70936fab7a841983dd0a0466cbc3
Yves Kerwyn <yves.kerwyn@greenitglobe.com>
2016-10-06 09:23:31 +0200

    Update inline documentation for deployment_test.py.

--- jumpscripts/healthchecks/deployment_test.py
+++ jumpscripts/healthchecks/deployment_test.py
@@ -1,10 +1,8 @@
 from JumpScale import j
 descr = """
-Tests every period if test VM exists and if exists it test write speed.
-Every 24hrs, test VM is recreated
-
-Generates warning if write speed is lower than 50 MiB / second
-
+Tests every predefined period (default 30 minutes) whether test VM exists and if exists it tests write speed. Every 24hrs, test VM is recreated.
+Result will be shown in the "Deployment Test" section of the Grid Portal / Status Overview / Node Status page.
+Generates warning if write speed is lower than 50 MiB / second.
 """
 
 organization = 'cloudscalers'
