36196169228133c9ff0090494e66018dbed6d31f
Yves Kerwyn <yves.kerwyn@greenitglobe.com>
2016-10-06 10:37:51 +0200

    Update inline documentation and error messages for raid_check.py.

--- jumpscripts/healthchecks/raid_check.py
+++ jumpscripts/healthchecks/raid_check.py
@@ -1,7 +1,8 @@
 from JumpScale import j
 
 descr = """
-Make sure all configured raid devices are still healthy
+Checks whether all configured RAID devices are still healthy.
+Result will be shown in the "Hardware" section of the Grid Portal / Status Overview / Node Status page.
 """
 
 organization = 'cloudscalers'
@@ -30,11 +31,11 @@ def action():
                     faultydisks.append(diskname)
             result = {'state': 'OK', 'category': category}
             if len(faultydisks) != 0:
-                msg = 'Raid device {} type {} has problems with disks ({})'.format(name, device['personality'], ', '.join(faultydisks))
+                msg = 'RAID device {} type {} has problems with disks ({})'.format(name, device['personality'], ', '.join(faultydisks))
                 result['uid'] = msg
                 result['state'] = 'ERROR'
             else:
-                msg = 'Raid device {} OK'.format(name)
+                msg = 'RAID device {} OK'.format(name)
             result['message'] = msg
             results.append(result)
 
