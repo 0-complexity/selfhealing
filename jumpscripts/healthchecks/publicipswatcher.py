fae05e97214789711daa9671c32cb4bf887b7608
Jo De Boeck <deboeck.jo@gmail.com>
2016-10-12 17:21:50 +0200

    Change public ips to externalnetwork

--- jumpscripts/healthchecks/publicipswatcher.py
+++ jumpscripts/healthchecks/publicipswatcher.py
@@ -23,36 +23,38 @@ def action():
     category = "Network"
     results = []
     ccl = j.clients.osis.getNamespace('cloudbroker')
-    pools = ccl.publicipv4pool.search(query={})[1:]  # ignore the count of search result.
+    pools = ccl.externalnetwork.search(query={})[1:]  # ignore the count of search result.
 
     for pool in pools:
-        network = netaddr.IPNetwork(pool['id'])
         gid = pool['gid']
-        pubips = pool['pubips']
-        pubips_count = len(pubips)
+        ips = pool['ips']
+        ips_count = len(ips)
         poolid = pool['id']
         usedips_count = ccl.cloudspace.count({'gid': gid, 'status': 'DEPLOYED'})
         for vm in ccl.vmachine.search({'nics.type': 'PUBLIC', 'status': {'$nin': ['ERROR', 'DESTROYED']}})[1:]:
             for nic in vm['nics']:
-                if nic['type'] == 'PUBLIC' and netaddr.IPNetwork(nic['ipAddress']).ip in network:
-                    usedips_count += 1
+                if nic['type'] == 'PUBLIC':
+                    tagObj = j.core.tags.getObject(nic['params'])
+                    if int(tagObj.tags.get('externalnetworkId', '0')) == pool['id']:
+                        usedips_count += 1
 
-        percent = (float(usedips_count) / (usedips_count + pubips_count)) * 100
+        percent = (float(usedips_count) / (usedips_count + ips_count)) * 100
         if percent > 95:
             results.append(dict(state='ERROR', category=category,
-                                message="Used public IPs on {poolid} passed the dangerous threshold. ({percent:.0f}%)"
+                                message="Used External IPs on {poolid} passed the dangerous threshold. ({percent:.0f}%)"
                                 .format(poolid=poolid, percent=percent)))
         elif percent > 80:
             results.append(dict(state='WARNING', category=category,
-                                message="Used public IPs on {poolid} passed the critical threshold. ({percent:.0f}%)"
+                                message="Used External IPs on {poolid} passed the critical threshold. ({percent:.0f}%)"
                                 .format(poolid=poolid, percent=percent)))
 
         else:
             results.append(dict(state='OK', category=category,
-                                message="Used public IPs on {poolid} ({percent:.0f}%)"
+                                message="Used External IPs on {poolid} ({percent:.0f}%)"
                                 .format(poolid=poolid, percent=percent)))
 
     return results
 
 if __name__ == "__main__":
-    print action()
+    import yaml
+    print(yaml.safe_dump(action(), default_flow_style=False))
