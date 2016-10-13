0eaec5dd55b513533e368b3e3a1c75507b6dc7d5
Ashraf Fouda <eng.ashraf.fouda@gmail.com>
2016-10-13 15:07:19 +0300

    Adds router os to network_virtual_stats (#26)
    Signed-off-by: Ashraf Fouda <foudaa@codescalers.com>

--- jumpscripts/collectors_stats/network_virtual_stats.py
+++ jumpscripts/collectors_stats/network_virtual_stats.py
@@ -1,4 +1,5 @@
 from JumpScale import j
+import xml.etree.ElementTree
 
 descr = """
 Gathers following network statistics from the virtual machines:
@@ -22,6 +23,30 @@ roles = ['cpunode']
 log = False
 
 
+def calc_network_data(interface_name, interface_type="interface"):
+    path = j.system.fs.joinPaths('/sys/class/net', interface_name, 'statistics')
+    result = {}
+    try:
+        bytes_sent = int(j.system.fs.fileGetContents(path + '/tx_bytes'))
+        bytes_recv = int(j.system.fs.fileGetContents(path + '/rx_bytes'))
+        packets_sent = int(j.system.fs.fileGetContents(path + '/tx_packets'))
+        packets_recv = int(j.system.fs.fileGetContents(path + '/rx_packets'))
+    except IOError as e:
+        if e.errno == 2:
+            return None
+    if interface_type == "routeros":
+        result['network.vfw.throughput.outgoing'] = int(round(bytes_sent / (1024.0 * 1024), 0))
+        result['network.vfw.throughput.incoming'] = int(round(bytes_recv / (1024.0 * 1024), 0))
+        result['network.vfw.packets.tx'] = packets_sent
+        result['network.vfw.packets.rx'] = packets_recv
+    else:
+        result['network.throughput.outgoing'] = int(round(bytes_sent / (1024.0 * 1024), 0))
+        result['network.throughput.incoming'] = int(round(bytes_recv / (1024.0 * 1024), 0))
+        result['network.packets.tx'] = packets_sent
+        result['network.packets.rx'] = packets_recv
+    return result
+
+
 def action():
     import libvirt
 
@@ -38,36 +63,43 @@ def action():
 
         vm = next(iter(vmcl.search({'referenceId': domain.UUIDString()})[1:]), None)
         if vm is None:
-            continue
+            if not domain.name().startswith("routeros"):
+                continue
+            interfaces_names = []
+            tree = xml.etree.ElementTree.fromstring(domain.XMLDesc())
+            targets = tree.findall('devices/interface/target')
 
-        for nic in vm['nics']:
-            mac = nic['macAddress']
-            path = j.system.fs.joinPaths('/sys/class/net', nic['deviceName'], 'statistics')
-            result = {}
+            for target in targets:
+                if not target.attrib['dev'].startswith('gwm'):
+                    interfaces_names.append(target.attrib['dev'])
 
-            now = j.base.time.getTimeEpoch()
-            try:
-                bytes_sent = int(j.system.fs.fileGetContents(path + '/tx_bytes'))
-                bytes_recv = int(j.system.fs.fileGetContents(path + '/rx_bytes'))
-                packets_sent = int(j.system.fs.fileGetContents(path + '/tx_packets'))
-                packets_recv = int(j.system.fs.fileGetContents(path + '/rx_packets'))
-            except IOError as e:
-                if e.errno == 2:
-                    continue  # file does not exist (anymore)
-                raise
+            for interface_name in interfaces_names:
+                now = j.base.time.getTimeEpoch()
+                result = calc_network_data(interface_name, "routeros")
+                if result is None:
+                    continue
+                all_results["vfw_%s" % (interface_name)] = result
 
-            result['network.throughput.outgoing'] = int(round(bytes_sent / (1024.0 * 1024), 0))
-            result['network.throughput.incoming'] = int(round(bytes_recv / (1024.0 * 1024), 0))
-            result['network.packets.tx'] = packets_sent
-            result['network.packets.rx'] = packets_recv
+                # send data to aggregator
+                for key, value in result.iteritems():
+                    key = "%s@virt.%s" % (key, interface_name)
+                    tags = 'gid:%d nid:%d nic:%s type:virtual' % (j.application.whoAmI.gid, j.application.whoAmI.nid, interface_name)
+                    aggregatorcl.measureDiff(key, tags, value, timestamp=now)
 
-            all_results["%s_%s" % (vm['id'], mac)] = result
+        else:
+            for nic in vm['nics']:
+                mac = nic['macAddress']
+                now = j.base.time.getTimeEpoch()
+                result = calc_network_data(nic['deviceName'])
+                if result is None:
+                    continue
+                all_results["%s_%s" % (vm['id'], mac)] = result
 
-            # send data to aggregator
-            for key, value in result.iteritems():
-                key = "%s@virt.%s" % (key, mac)
-                tags = 'gid:%d nid:%d nic:%s mac:%s type:virtual' % (j.application.whoAmI.gid, j.application.whoAmI.nid, nic['deviceName'], mac)
-                aggregatorcl.measureDiff(key, tags, value, timestamp=now)
+                # send data to aggregator
+                for key, value in result.iteritems():
+                    key = "%s@virt.%s" % (key, mac)
+                    tags = 'gid:%d nid:%d nic:%s mac:%s type:virtual' % (j.application.whoAmI.gid, j.application.whoAmI.nid, nic['deviceName'], mac)
+                    aggregatorcl.measureDiff(key, tags, value, timestamp=now)
 
     return all_results
 
