247f3f7c875345af3ffcfdfe6bf81b7f1350cfbb
Jo De Boeck <deboeck.jo@gmail.com>
2016-12-12 10:32:20 +0200

    routeros_check: Add links to objects
    Fixes: #0-complexity/openvcloud/issues/559

--- jumpscripts/healthchecks/routeros_check.py
+++ jumpscripts/healthchecks/routeros_check.py
@@ -25,18 +25,22 @@ def action():
     def checkros(c):
         vcl = j.clients.osis.getNamespace('vfw')
         vfwid = '{gid}_{networkId}'.format(gid=c['gid'], networkId=c['networkId'])
+        roslink = '[{vfwid}|/cbgrid/private network?id={id}&gid={gid}]'.format(
+            vfwid=vfwid, id=c['networkId'], gid=c['gid'])
+        spacelink = '[{name}|/cbgrid/cloud space?id={id}]'.format(name=c['name'], id=c['id'])
         if not vcl.virtualfirewall.exists(vfwid):
-            return dict(state='ERROR', category=category, message="RouterOS doesn't exist on {vfwid} {csname}".format(vfwid=vfwid, csname=c['name']))
+            return dict(state='ERROR', category=category, message="RouterOS {vfwid} doesn't exist on {spacelink}".format(vfwid=vfwid, spacelink=spacelink))
         vfw = vcl.virtualfirewall.get(vfwid)
         try:
             client = j.clients.routeros.get(vfw.host, vfw.username, vfw.password)
             ok = client.ping('8.8.8.8')
             if not ok:
-                return dict(state='ERROR', category=category, message="Couldn't ping 8.8.8.8 {vfwid} {csname}".format(vfwid=vfwid, csname=c['name']))
+                return dict(state='ERROR', category=category, message="Couldn't ping 8.8.8.8 on {roslink} for {spacelink}".format(roslink=roslink, spacelink=spacelink))
             return None
         except Exception as e:
             print("Failed to connect to {vfwid} {csname} error {err}".format(vfwid=vfwid, csname=c['name'], err=e))
-            return dict(state='ERROR', category=category, message="RouterOS died on {vfwid} {csname}".format(vfwid=vfwid, csname=c['name']))
+            return dict(state='ERROR', category=category,
+                        message="RouterOS {roslink} on {spacelink} died".format(roslink=roslink, spacelink=spacelink))
 
     pool = Pool(10)
     results = pool.map(checkros, cloudspaces)
