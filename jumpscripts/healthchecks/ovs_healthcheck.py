78a4ecc4631a4e59af8403019c6bf2a73276f59d
Jo De Boeck <deboeck.jo@gmail.com>
2016-12-14 15:07:11 +0200

    Changes to ovs_healthcheck

--- jumpscripts/healthchecks/ovs_healthcheck.py
+++ jumpscripts/healthchecks/ovs_healthcheck.py
@@ -36,16 +36,16 @@ def action():
     from ovs.extensions.healthcheck.alba.alba_health_check import AlbaHealthCheck
     from ovs.log.healthcheck_logHandler import HCLogHandler
 
-    module = ''
+    module = {'name': ''}
 
     def failure(self, msg, test_name=None):
-        results.append({'message': msg, 'uid': test_name, 'category': module, 'state': 'ERROR'})
+        results.append({'message': msg, 'uid': test_name, 'category': module['name'], 'state': 'ERROR'})
 
     def success(self, msg, test_name=None):
-        results.append({'message': msg, 'uid': test_name, 'category': module, 'state': 'OK'})
+        results.append({'message': msg, 'uid': test_name, 'category': module['name'], 'state': 'OK'})
 
     def warning(self, msg, test_name=None):
-        results.append({'message': msg, 'uid': test_name, 'category': module, 'state': 'WARNING'})
+        results.append({'message': msg, 'uid': test_name, 'category': module['name'], 'state': 'WARNING'})
 
     HCLogHandler.failure = failure
     HCLogHandler.success = success
@@ -53,55 +53,20 @@ def action():
     logger = HCLogHandler(print_progress=False)
     results = []
 
-    alba = AlbaHealthCheck()
-    alba.module = "Alba Module"
-    arakoon = ArakoonHealthCheck()
-    arakoon.module = "Arakoon Module"
-    ovs = OpenvStorageHealthCheck()
-    ovs.module = "OVS Module"
-    volumedriver = VolumedriverHealthCheck()
-    volumedriver.module = "Volumedriver Module"
+    def run(check, name):
+        module['name'] = name
+        try:
+            check.run(logger)
+        except Exception as e:
+            eco = j.errorconditionhandler.processPythonExceptionObject(e)
+            msg = 'Failure in check see [eco|/grid/error condition?id={}]'.format(eco.guid)
+            results.append({'message': msg, 'category': name, 'state': 'ERROR'})
 
-    def check_arakoon():
-        """
-        Checks all critical components of Arakoon
-        """
-        arakoon.check_arakoons()
+    run(OpenvStorageHealthCheck, 'OpenvStorage')
+    run(ArakoonHealthCheck, 'Araoon')
+    run(VolumedriverHealthCheck, 'Volumedriver')
+    run(AlbaHealthCheck, 'Alba')
 
-    def check_openvstorage():
-        """
-        Checks all critical components of Open vStorage
-        """
-        ovs.get_local_settings(logger)
-        ovs.check_ovs_workers(logger)
-        ovs.check_ovs_packages(logger)
-        ovs.check_required_ports(logger)
-        ovs.get_zombied_and_dead_processes(logger)
-        ovs.check_required_dirs(logger)
-        ovs.check_size_of_log_files(logger)
-        ovs.check_if_dns_resolves(logger)
-        ovs.check_model_consistency(logger)
-        ovs.check_for_halted_volumes(logger)
-        ovs.check_filedrivers(logger)
-
-    def check_volumedriver():
-        volumedriver.check_dtl(logger)
-        volumedriver.check_volumedrivers(logger)
-
-    def check_alba():
-        """
-        Checks all critical components of Alba
-        """
-        alba.check_alba(logger)
-
-    module = 'OpenvStorage'
-    check_openvstorage()
-    module = 'Arakoon'
-    # check_arakoon()
-    module = 'Volumedrver'
-    check_volumedriver()
-    module = 'Alba'
-    check_alba()
     return results
 
 
