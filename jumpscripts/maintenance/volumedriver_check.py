ea62bab7069c32088bbc4580a4022b621810f99a
Muhamad Azmy <muhamad.azmy@codescalers.com>
2017-01-17 14:59:00 +0200

    WIP: Check avg memory consumption for the volumedriver process

--- jumpscripts/maintenance/volumedriver_check.py
+++ jumpscripts/maintenance/volumedriver_check.py
@@ -1,5 +1,6 @@
 from JumpScale import j
 import psutil
+import re
 
 descr = """
 This script make sure any rouge volumedriver is killed by checking its threads count and memory consumption
@@ -29,6 +30,11 @@ VOLUMEDRIVER_NAME = 'volumedriver_fs'
 
 
 def check_over_memory(value):
+    # memory can be None if stats collection didn't report any values yet.
+    # we handle this case here for simplicity.
+    if value is None:
+        return False
+
     # formula to find if this memory value is exceeding the threshold
     vm = psutil.virtual_memory()
     if float(value) / vm.total >= MEMORY_THRESHOLD:
@@ -48,15 +54,36 @@ def clean_volumedriver(ps):
     pass
 
 
+def get_memory_avg(agg, ps):
+    cmd = ' '.join(ps.cmdline())
+    m = re.search(r'--mountpoint /mnt/(\w+)', cmd)
+    if m is None:
+        raise RuntimeError('cannot find the pool mount point for volumedriver pid: %s', ps.pid)
+
+    pool = m.group(1)
+    key = 'process.memory.vmrss@phys.%d.%d.%s_%s' % (j.application.whoAmI.gid, j.application.whoAmI.nid, VOLUMEDRIVER_NAME, pool)
+    stat = agg.statGet(key)
+
+    if stat is None:
+        return None
+
+    return stat.m_avg
+
+
 def action():
+    rediscl = j.clients.redis.getByInstance('system')
+    aggregatorcl = j.tools.aggregator.getClient(rediscl, "%s_%s" % (j.application.whoAmI.gid, j.application.whoAmI.nid))
+
     # - Find all volumedriver processe
     for process in psutil.process_iter():
         if process.name() != VOLUMEDRIVER_NAME:
             continue
 
+        mem = get_memory_avg(aggregatorcl, process)
+
         # check number of threads or memory
         if check_over_threads(len(process.threads())) or \
-                check_over_memory(process.memory_info().rss):
+                check_over_memory(mem):
             # volumedriver must be cleaned up
             clean_volumedriver(process)
 
