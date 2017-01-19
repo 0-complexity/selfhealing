9fddf9b788eb8d7d5b7c896643080415761bbd6c
abdulrahmantkhalifa <abdulrahman.t.khalifa@gmail.com>
2017-01-19 20:30:19 +0200

    adjustments to fair use script to use tags instead of redis

--- jumpscripts/healthchecks/vm_cpu_fair_use_policy.py
+++ jumpscripts/healthchecks/vm_cpu_fair_use_policy.py
@@ -1,11 +1,14 @@
 from JumpScale import j
 
 descr = """
-Checks average memory and CPU usage/load. If average per hour is higher than expected an error condition is thrown.
+Checks cpu time and quratines machine according to fair use policy .
 
-For both memory and CPU usage throws WARNING if more than 80% used and throws ERROR if more than 95% used.
+If average per 5 min is higher than threshold a warning is sent via email.
 
-Result will be shown in the "System Load" section of the Grid Portal / Status Overview / Node Status page.
+if over use presists for longer than warntime  the machine is quarantined to a limit,
+this is done for a quarantine time and then unquarantined .
+
+if quarantined again after unquarantine quarantine time doubles for each time.
 """
 
 organization = "jumpscale"
@@ -24,11 +27,7 @@ queue = 'process'
 roles = ['node']
 
 
-quarantinetime = 60
-warntime = 30
-
-
-def action(warntime=300, quarantinetime=600):
+def action(warntime=300, quarantinetime=600, threshold=0.8):
     import libvirt
     import json
     from CloudscalerLibcloud.utils.Dispatcher import Dispatcher
@@ -38,81 +37,109 @@ def action(warntime=300, quarantinetime=600):
     d = Dispatcher()
     connection = libvirt.open()
     rediscl = j.clients.redis.getByInstance('system')
+    cbcl = j.clients.osis.getNamespace('cloudbroker')
     
     # for demo use
     def emailsend(msg):
-        j.clients.email.send(toaddrs, fromaddr, subject, message, files=None)
+        # j.clients.email.send(toaddrs, fromaddr, subject, message, files=None)
+        print("msg == > ", msg)
 
-    def quarantine(quarantined, domain_dict, qt):
-        d.quarantine_vm(domain.ID())
+    def quarantine(quarantined, vm_dict, qt):
+        d.quarantine_vm(domain.UUIDString())
         emailsend('quarantine')
-        quarantined[domain_id] = {"warntimestart": domain_dict['warntimestart'], "warntime": warntime,
-                                  "quarantinetimestart": j.base.time.getTimeEpoch(), "quarantinetime": qt,
-                                  "quarantinetimelegacy": None}
-        rediscl.set(key, json.dumps(quarantined))
-    
-    def unquaranetine(quarantined, domain_dict):
+        tags.tagSet("warntimestart", tags.tagGet('warntimestart'))
+        tags.tagSet("warntime", warntime)
+        tags.tagSet("quarantinetimestart", j.base.time.getTimeEpoch())
+        tags.tagSet("quarantinetime", qt)
+        if tags.tagExists("quarantinetimelegacy"):
+            tags.tagDelete("quarantinetimelegacy")
+        tags.tagSet("warned", True)
+        vm_dict.tags = str(tags)
+        cbcl.vmachine.updateSearch({'id': vm_dict.id}, {'$set': {'tags': str(tags)}})
+
+    def unquaranetine(quarantined, vm_dict):
         emailsend("unquarantine")
-        quarantined[domain_id] = {"warntimestart": None, "warntime": domain_dict['warntime'],
-                                  "quarantinetimestart": domain_dict['quarantinetimestart'],
-                                  "quarantinetime": None, "quarantinetimelegacy": domain_dict['quarantinetimelegacy']}
-        d.unquarantine_vm(domain.ID())
-        rediscl.set(key, json.dumps(quarantined))
-        
+        tags.tagDelete("warntimestart")
+        tags.tagSet("warntime", tags.tagGet('warntime'))
+        tags.tagSet("quarantinetimestart", tags.tagGet('quarantinetimestart'))
+        tags.tagSet("quarantinetimelegacy", tags.tagGet('quarantinetime'))
+        if tags.tagExists("quarantinetime"):
+            tags.tagDelete("quarantinetime")
+        if tags.tagExists("warned"):
+            tags.tagDelete("warned")
+        d.unquarantine_vm(domain.UUIDString())
+        vm_dict.tags = str(tags)
+        cbcl.vmachine.updateSearch({'id': vm_dict.id}, {'$set': {'tags': str(tags)}})
+
     def warn(quarantined):
         emailsend("warn")
-        quarantined[domain_id] = {"warntimestart": j.base.time.getTimeEpoch(), "warntime": warntime,
-                                  "quarantinetimestart": 0, "quarantinetime": None, "quarantinetimelegacy": None}
-        rediscl.set(key, json.dumps(quarantined))
+        tags.tagSet("warntimestart", j.base.time.getTimeEpoch())
+        tags.tagSet("warntime", warntime)
+        tags.tagSet("quarantinetimestart", 0)
+        if tags.tagExists("quarantinetime"):
+            tags.tagDelete("quarantinetime")
+        if tags.tagExists("quarantinetimelegacy"):
+            tags.tagDelete("quarantinetimelegacy")
+        tags.tagSet("warned", True)
+        vm_dict.tags = str(tags)
+        cbcl.vmachine.updateSearch({'id': vm_dict.id}, {'$set': {'tags': str(tags)}})
 
     # list all vms running in this node
     domains = connection.listAllDomains()
-    
     # list all quarantined
     # check if quartined list exists if not create it
     q_string = rediscl.get(key)
-    if q_string != '':
+    if q_string:
         quarantined = json.loads(q_string)
     else:
         quarantined = {}
 
     for domain in domains:
-        domain_id = str(domain.ID())
+        if not domain.name().startswith("vm-"):
+            continue
+        domain_id = domain.name().strip('vm-')
+        vm_dict = cbcl.vmachine.get(int(domain_id))
+        tags = j.core.tags.getObject(vm_dict.tags)
         # calculate cputime_avg
         aggregatorcl = j.tools.aggregator.getClient(rediscl, "%s_%s" % (j.application.whoAmI.gid,
                                                                         j.application.whoAmI.nid))
-        stats = aggregatorcl.statGet("machine.CPU.utilisation@virt.%s" % (j.application.whoAmI.gid,
-                                                                          j.application.whoAmI.nid,
-                                                                          domain_id))
+        stats = aggregatorcl.statGet("machine.CPU.utilisation@virt.%s" % domain_id)
+        if not stats:
+            continue
         cputime = stats.m_last
         cputime_avg = cputime / 300
-
-        if cputime_avg >= 0.8:
-            if '%s' % domain.ID() in quarantined.keys():
-                domain_dict = quarantined[domain_id]
-                if not domain_dict[0]:
+        if tags.tagExists('warned'):
+            if cputime_avg < threshold:
+                if tags.tagExists('quarantinetimestart') and tags.tagGet('quarantinetimestart') != '0':
+                    quarantine_timer = j.base.time.getTimeEpoch() - int(tags.tagGet('quarantinetimestart'))
+                    if tags.tagExists('quarantinetime') and quarantine_timer >= int(tags.tagGet('quarantinetime')):
+                        unquaranetine(quarantined, vm_dict)
+                        continue
+                    else:
+                        continue
+            if cputime_avg >= threshold:
+                if not tags.tagExists('warntimestart'):
                     warn(quarantined)
                     continue
-                warn_timer = j.base.time.getTimeEpoch() - domain_dict[0]
-                if warn_timer >= domain_dict[1]:
-                    if domain_dict[2]:
-                        quarantine_timer = j.base.time.getTimeEpoch() - domain_dict[2]
-                        if quarantine_timer >= domain_dict[3]:
-                            unquaranetine(quarantined, domain_dict)
+                warn_timer = j.base.time.getTimeEpoch() - int(tags.tagGet('warntimestart'))
+                if warn_timer >= int(tags.tagGet('warntime')):
+                    if tags.tagExists('quarantinetimestart') and tags.tagGet('quarantinetimestart') != '0':
+                        quarantine_timer = j.base.time.getTimeEpoch() - int(tags.tagGet('quarantinetimestart'))
+                        if tags.tagExists('quarantinetime') and quarantine_timer >= int(tags.tagGet('quarantinetime')):
+                            unquaranetine(quarantined, vm_dict)
                             continue
                         else:
                             continue
                     else:
                         qt = quarantinetime
-                        if domain_dict[4]:
-                            qt = domain_dict[4] * 2
-                        quarantine(quarantined, domain_dict, qt)
+                        if tags.tagExists('quarantinetimelegacy'):
+                            qt = int(tags.tagGet('quarantinetimelegacy')) * 2
+                        quarantine(quarantined, vm_dict, qt)
                         continue
                 else:
                     continue
-            else:
-                warn(quarantined)
+        else:
+            warn(quarantined)
 
 if __name__ == '__main__':
-    import ipdb; ipdb.set_trace()
-    action(warntime=30, quarantinetime=60)
+    action(warntime=30, quarantinetime=60, threshold=0.001)
