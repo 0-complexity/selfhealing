6bdeac9f86be2017908b681bffaa8277c68d86ad
Jo De Boeck <deboeck.jo@gmail.com>
2017-02-07 10:56:56 +0200

    Restart stopped ays services
    Signed-off-by: Jo De Boeck <deboeck.jo@gmail.com>

--- jumpscripts/healthchecks/ays_process_check.py
+++ jumpscripts/healthchecks/ays_process_check.py
@@ -26,21 +26,35 @@ def action():
     results = list()
     for ays in j.atyourservice.findServices():
         if not ays.getProcessDicts():
-                continue
+            continue
         result = dict()
         result['state'] = 'OK'
         result['message'] = "Process %s:%s:%s " % (ays.domain, ays.name, ays.instance)
         result['category'] = 'AYS Process'
         if not ays.actions.check_up_local(ays, wait=False):
-            message = "Process %s:%s:%s is halted" % (ays.domain, ays.name, ays.instance)
-            j.errorconditionhandler.raiseOperationalWarning(message, 'monitoring')
-            result['state'] = 'HALTED'
-            result['message'] = message
-            result['uid'] = message
-            result['category'] = 'AYS Process'
+            message = "Restarted process %s:%s:%s" % (ays.domain, ays.name, ays.instance)
+            eco_tags = j.core.tags.getObject()
+            eco_tags.tagSet('domain', ays.domain)
+            eco_tags.tagSet('name', ays.name)
+            eco_tags.tagSet('instance', ays.instance)
+            j.errorconditionhandler.raiseOperationalWarning(
+                message=message,
+                category='selfhealing',
+                tags=str(eco_tags)
+            )
+            ays.start()
+
+            if not ays.actions.check_up_local(ays, wait=True):
+                message = "Process %s:%s:%s is halted" % (ays.domain, ays.name, ays.instance)
+                j.errorconditionhandler.raiseOperationalWarning(message, 'monitoring')
+                result['state'] = 'HALTED'
+                result['message'] = message
+                result['uid'] = message
         results.append(result)
 
     return results
 
+
 if __name__ == '__main__':
-    action()
+    import yaml
+    print(yaml.safe_dump(action(), default_flow_style=False))
