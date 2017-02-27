d1d29cfc4a62869df3871fef94700632e41d4b37
Jo De Boeck <deboeck.jo@gmail.com>
2017-02-27 14:59:04 +0200

    Change node_maintenance to run on master
    This is required sicne there is no portal client on the controller
    
    Signed-off-by: Jo De Boeck <deboeck.jo@gmail.com>

--- jumpscripts/maintenance/node_maintenance.py
+++ jumpscripts/maintenance/node_maintenance.py
@@ -10,7 +10,7 @@ author = "chaddada@greenitglobe.com"
 category = "monitor.maintenance"
 license = "bsd"
 version = "1.0"
-period = 60  # always in sec
+period = 180  # always in sec
 timeout = period * 0.2
 startatboot = True
 order = 1
@@ -18,26 +18,27 @@ enable = True
 async = True
 log = False
 queue = 'process'
-roles = ['controller']
+roles = ['master']
 
 
 def action():
     acl = j.clients.agentcontroller.get()
     sessions = acl.listSessions()
     ccl = j.clients.osis.getNamespace('cloudbroker')
-    stacks = ccl.stack.search({'status': 'ENABLED', 'gid': j.application.whoAmI.gid})[1:]
+    stacks = ccl.stack.search({'status': 'ENABLED'})[1:]
     pcl = j.clients.portal.getByInstance('main')
     for stack in stacks:
         for key, value in sessions.iteritems():
             if int(key.split('_')[1]) == int(stack['referenceId']):
                 ti = int(time.time() - value[0])
                 if ti > 300:
-                    pcl.actors.cloudbroker.computenode.maintenance(id=stack['id'], gid=j.application.whoAmI.gid, vmaction='move')
+                    pcl.actors.cloudbroker.computenode.maintenance(id=stack['id'], gid=stack['gid'], vmaction='move')
                     eco = j.errorconditionhandler.getErrorConditionObject(msg='Node: %s is put in maintenance mode' % (stack['referenceId']),
                                                                           msgpub='Node: %s is put in maintenance mode' % (stack['referenceId']),
                                                                           category='selfhealing', level=3,
                                                                           type="MONITORING")
                     eco.nid = int(stack['referenceId'])
+                    eco.gid = stack['gid']
                     eco.process()
 
 
