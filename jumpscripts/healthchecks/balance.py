583d2f18428222224d37dc4cf481f19f53fb6745
Nicolas Krassas <krasn@swissns.ch>
2017-09-12 09:33:51 +0300

    Create balance.py (#67)
    Create maintenance script which balances btrfs

---
+++ jumpscripts/healthchecks/balance.py
@@ -0,0 +1,28 @@
+from JumpScale import j
+
+descr = """
+This script executes btrfs balance command every 5 days at 4 in the morning random .
+"""
+
+organization = "jumpscale"
+author = "support@gig.tech"
+category = "monitor.maintenance"
+license = "bsd"
+version = "1.0"
+interval = (2 * j.application.whoAmI.nid) % 30
+period = "%s, '*/4', '*/5' * *" % (interval)
+startatboot = False
+order = 1
+enable = True
+async = True
+log = False
+queue = 'process'
+roles = ['cpunode', 'storagenode', 'storagedriver', ]
+
+
+
+def action():
+    j.system.process.execute('nohup /bin/btrfs balance start -dusage=80 / > /dev/null &', useShell=True)
+
+if __name__ == '__main__':
+    action()
