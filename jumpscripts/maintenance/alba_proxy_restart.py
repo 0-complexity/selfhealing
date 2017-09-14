f34a7e989eb44d672eb5ca47e680f7df82901b67
Nicolas Krassas <krasn@swissns.ch>
2017-09-14 22:40:02 +0300

    Alba restart daily (#71)
    * Alba restart daily

---
+++ jumpscripts/maintenance/alba_proxy_restart.py
@@ -0,0 +1,31 @@
+from JumpScale import j
+
+descr = """
+This script alba proxy restart command every days at 7 in the morning .
+"""
+
+organization = "jumpscale"
+author = "support@gig.tech"
+category = "monitor.maintenance"
+license = "bsd"
+version = "1.0"
+interval = (2 * j.application.whoAmI.nid) % 30
+period = "%s, 7, * * *" % (interval)
+startatboot = False
+order = 1
+enable = True
+async = True
+log = False
+queue = 'process'
+roles = ['storagenode']
+
+
+def action():
+    for name, status in j.system.platform.ubuntu.listServices().items():
+        if 'ovs-albaproxy' in name and status == 'enabled':
+            print('Restarting {}'.format(name))
+            j.system.platform.ubuntu.restartService(name)
+
+
+if __name__ == '__main__':
+    action()
