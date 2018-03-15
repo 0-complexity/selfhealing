20ee0fa5e0b19ee2ca0a8cb2f8804bf055999ffa
Ashraf Fouda <ashraf.m.fouda@gmail.com>
2018-03-15 11:08:04 +0200

    Adds pin ovs_process to a cgroup (#176)
    Signed-off-by: Ashraf Fouda <ashraf.m.fouda@gmail.com>

---
+++ jumpscripts/maintenance/ovs_cgroup.py
@@ -0,0 +1,44 @@
+from JumpScale import j
+
+descr = """
+This script creates an ovs cgroup and assig ovs related processes to it
+"""
+
+organization = "jumpscale"
+author = "foudaa@greenitglobe.com"
+category = "monitor.maintenance"
+license = "bsd"
+version = "1.0"
+period = "0 * * * *"
+startatboot = False
+order = 1
+enable = True
+async = True
+log = False
+queue = 'process'
+roles = ['cpunode', 'storagedriver']
+timeout = 60
+
+
+def action():
+    j.system.process.run('apt-get -y install cgroup-bin cgroup-lite cgroup-tools', showOutput=False, captureOutput=True, stopOnError=False)
+    scl = j.clients.osis.getNamespace('system')
+    grid = scl.grid.get(j.application.whoAmI.gid)
+    limits = grid.settings['limits']
+    mem = limits['ovs']['memory'] # should be in Mega
+    cpu = limits['ovs']['cpu']
+    j.system.platform.cgroups.create('ovs')
+    j.system.platform.cgroups.set_mem_limit('ovs', mem)
+    j.system.platform.cgroups.set_cpu_cores('ovs', cpu)
+
+    pids = []
+    for name, status in j.system.platform.ubuntu.listServices().items():
+        if status == 'enabled' and 'ovs' in name or 'arakoon' in name or 'alba' in name:
+            pid = j.system.platform.ubuntu.getServicePID(name.strip())
+            if pid:
+                pids.append(pid)
+    if pids:
+        j.system.platform.cgroups.add_processes('ovs', pids)
+
+if __name__ == '__main__':
+    action()
